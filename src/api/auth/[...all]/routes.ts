25e3c52adf7bfbf5f23d5d769ad9e6acb5affbac
lucaserm <lucasedumacedo@gmail.com>
2025-08-06 10:29:16 -0400

    chore: add better auth setup

--- src/api/auth/[...all]/routes.ts
+++ src/api/auth/[...all]/routes.ts
@@ -1,6 +1,5 @@
 import { toNextJsHandler } from "better-auth/next-js";
 
-import 
-  import { auth } from "@/lib/auth";
+import { auth } from "@/lib/auth";
 
 export const { GET, POST } = toNextJsHandler(auth.handler);
