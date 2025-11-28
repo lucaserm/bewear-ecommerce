0d76dcb478e3d11f42c815cfa52a93d9e2e6d001
Lucas Macedo <lucasedumacedo@gmail.com>
2025-11-27 22:42:57 -0400

    feat: improving with github workflow

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,7 +2,7 @@ import { loadStripe } from "@stripe/stripe-js";
 
 import { env } from "@/env";
 
-const configValue: string = env.NEXT_PUBLIC_STRIPE_KEY as string;
+const configValue = env.NEXT_PUBLIC_STRIPE_KEY;
 export async function getStripeJs() {
   return await loadStripe(configValue);
 }
